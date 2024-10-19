8da3c5d7c2eb482aada9c791cc0504a5d527aa71
Kikes9900 <mykee5511@gmail.com>
2024-10-19 11:54:12 -0600

    saving new changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ import taskApp from "../media/TaskVid.mp4";
 const Home = () => {
     return (
         <div className="italic text-center font-medium500Italic mt-9">
-            <h3 className="text-2xl mb-5">Software Developer</h3>
-            <p className="w-[400px] m-auto">My name is Mykee Flores, Founder of Web Creator. I am a Army veteran who found his path in Web Development. Explore my website to learn more about me and see my work. Reach me in the contact page for a project or more info.</p>
-            <div >
-                <h3>Full Stack web applications</h3>
+            <h3 className="text-4xl mb-5"><span className="border-b-2 border-black inline-block">Software Developer</span></h3>
+            <p className="w-[400px] m-auto text-2xl mb-[50px]">My name is Mykee Flores, I build websites for clients, and I am a Army veteran who found his path in Web Development. Explore my website to learn more about me and see my work. Reach me in the contact page for a project or more info.</p>
+            <div>
+                <h3 className="text-4xl">Full Stack web applications</h3>
                 <div className="flex flex-row justify-around items-center w-full max-w-screen-lg mx-auto">
                     <figure className="mx-[40px] my-9">
                         <h6>A movie website</h6>
@@ -21,6 +21,9 @@ const Home = () => {
                         <figcaption>The website has basic CRUD operations and also allows users to effectively view and manage tasks. Python, JavaScript, and the Django framework were used building this web application.</figcaption>
                     </figure>
                 </div>
+            </div>
+            <div>
+                <button>Reach me here</button>
             </div>
         </div>
     )
