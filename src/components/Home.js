c63cca2ba135a690d0036bc47e6773ac154f95e1
Kikes9900 <mykee5511@gmail.com>
2024-10-19 13:22:36 -0600

    saving changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,25 @@
 import React from "react";
 import pyMovie from "../media/MovieMaker.mp4";
 import taskApp from "../media/TaskVid.mp4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
     return (
         <div className="italic text-center font-medium500Italic mt-9">
             <h3 className="text-4xl mb-5"><span className="border-b-2 border-black inline-block">Software Developer</span></h3>
+            <div>
             <p className="w-[400px] m-auto text-2xl mb-[50px]">My name is Mykee Flores, I build websites for clients, and I am a Army veteran who found his path in Web Development. Explore my website to learn more about me and see my work. Reach me in the contact page for a project or more info.</p>
             <div>
+                <p>Reach me here</p>
+                <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer" className="flex items-center text-blue-700 hover:text-blue-900">
+                        <FontAwesomeIcon icon={faLinkedin} className="mr-2" /> {/* Add margin for spacing */}
+                        LinkedIn
+                </a>
+                <a href="" target="_blank"></a>
+            </div>
+            </div>            <div>
                 <h3 className="text-4xl">Full Stack web applications</h3>
                 <div className="flex flex-row justify-around items-center w-full max-w-screen-lg mx-auto">
                     <figure className="mx-[40px] my-9">
